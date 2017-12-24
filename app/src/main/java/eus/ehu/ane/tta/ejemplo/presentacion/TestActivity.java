25b06214844a252406f6701d7d966db1f5aece13
Ane Perez de Nanclares <aperezdenanc005@ikasle.ehu.eus>
2017-12-24 12:06:45 +0100

    El test indicando si has fallado o no + ayuda en caso de fallo

--- app/src/main/java/eus/ehu/ane/tta/ejemplo/presentacion/TestActivity.java
+++ app/src/main/java/eus/ehu/ane/tta/ejemplo/presentacion/TestActivity.java
@@ -1,19 +1,26 @@
 package eus.ehu.ane.tta.ejemplo.presentacion;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.webkit.WebView;
 
 import eus.ehu.ane.tta.ejemplo.R;
+
+import static eus.ehu.ane.tta.ejemplo.R.id.button_send_help;
+import static eus.ehu.ane.tta.ejemplo.R.id.button_send_test;
 
 public class TestActivity extends AppCompatActivity {
 
     String pregunta="¿Cuál de las siguientes opciones NO se indica en el fichero de manifiesto?";
     String[] respuestas={"Versión de la aplicación","Listado de componentes de la aplicación","Opciones del menu de ajustes","Nivel mínimo de la API Android requerida","Nombre del paquete java de la aplicación"};
-
+    String advise="<html><body>The manifest describes the <b>componentes of the applicaction:</b> the activities, services, broadcast receivers and content providers that ...</body></html>";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +44,42 @@ public class TestActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v)
                 {
-                    findViewById(R.id.button_send_test).setVisibility(View.VISIBLE);
+                    findViewById(button_send_test).setVisibility(View.VISIBLE);
                 }
 
             });
         }
+    }
+
+    public void comprobarTest(View view)
+    {
+        int correct=2;
+        RadioGroup group=(RadioGroup)findViewById(R.id.test_choices);
+        //Quitamos el boton de ENVIAR
+        ViewGroup layout=(ViewGroup)findViewById(R.id.test_layout);
+        layout.removeView(findViewById(button_send_test));
+
+        group.getChildAt(correct).setBackgroundColor(Color.GREEN);
+        int selected=group.indexOfChild(group.findViewById(group.getCheckedRadioButtonId()));
+        if(selected!=correct)
+        {
+            group.getChildAt(selected).setBackgroundColor(Color.RED);
+            Toast.makeText(this,R.string.test_opcion_erronea,Toast.LENGTH_LONG).show();
+            findViewById(button_send_help).setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            Toast.makeText(this,R.string.test_opcion_correcta,Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void textoAyuda(View view)
+    {
+        WebView web=new WebView(this);
+        ViewGroup layout=(ViewGroup)findViewById(R.id.test_layout);
+        web.loadData(advise,"text/html",null);
+        web.setBackgroundColor(Color.TRANSPARENT);
+        web.setLayerType(WebView.LAYER_TYPE_SOFTWARE,null);
+        layout.addView(web);
     }
 }
